a0c38538eccff49770e5611df651e4b276d4b977
SuperGamersGames <44580811+SuperGamersGames@users.noreply.github.com>
2025-04-07 12:23:34 -0500

    Delta Time

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0 # Delta Time
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,7 @@ def main():
 
         pygame.Surface.fill(screen,pygame.Color(0,0,0))
         pygame.display.flip()
+        dt = time.tick()/1000
 
 if __name__ == "__main__":
     main()
