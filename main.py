9577e59a89c1b520ec431e9057428ba47f14729b
SuperGamersGames <44580811+SuperGamersGames@users.noreply.github.com>
2025-04-07 12:39:51 -0500

    Player Class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -10,12 +11,17 @@ def main():
     time = pygame.time.Clock()
     dt = 0 # Delta Time
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         pygame.Surface.fill(screen,pygame.Color(0,0,0))
+
+        player.draw(screen)
+
         pygame.display.flip()
         dt = time.tick()/1000
 
