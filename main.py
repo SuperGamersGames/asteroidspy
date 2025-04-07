c5f4f154f8328f34bc59318ee2c958190a4bf5d0
SuperGamersGames <44580811+SuperGamersGames@users.noreply.github.com>
2025-04-07 13:01:51 -0500

    Add Groupables

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time = pygame.time.Clock()
     dt = 0 # Delta Time
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable,drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -20,11 +26,12 @@ def main():
 
         pygame.Surface.fill(screen,pygame.Color(0,0,0))
 
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
 
         pygame.display.flip()
-        dt = time.tick(60)/1000
+        dt = time.tick(GAME_FPS)/1000
 
 if __name__ == "__main__":
     main()
